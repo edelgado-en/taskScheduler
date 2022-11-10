a83d426d01f4fd728b0ca03ba9870be7b5d6d473
edelgado-git <enriquedelgado806@gmail.com>
2022-11-10 07:47:30 -0500

    Added ability to logout

--- TaskScheduler/TaskScheduler/TasksViewController.swift
+++ TaskScheduler/TaskScheduler/TasksViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class TasksViewController: UIViewController {
 
@@ -16,6 +17,17 @@ class TasksViewController: UIViewController {
     }
     
 
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    
+    }
+    
     /*
     // MARK: - Navigation
 
