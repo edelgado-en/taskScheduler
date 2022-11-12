5270c2bdddee4ca54ca7b664a73d75f8e02d41a5
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2022-11-11 22:18:01 -0500

    Reorganized File - Still same issue
    line 52 now holds the error. It still says I'm indexing out of bounds but I am sure that my query is working. The print statements on line 32 and 34 return a value greater than 0 which means I am reading from the database correctly.

--- TaskScheduler/TaskScheduler/TasksViewController.swift
+++ TaskScheduler/TaskScheduler/TasksViewController.swift
@@ -12,6 +12,31 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     var jobs = [PFObject]() //an array to hold jobs
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //query data from the database
+        //issue is in here.. list is still empty at the end of query
+        let query = PFQuery(className:"Job")
+        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in if objects != nil {
+            print("OBJECTS != NIL")
+            self.jobs = objects!
+            print(self.jobs.count)
+            self.tableView.reloadData()
+            print(self.jobs.count)
+            }
+        }
+    }
+
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return a number of rows. Also can be used as an iterator I guess
         return 1
@@ -19,18 +44,18 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //getting each individual cell
-        let job = jobs[indexPath.row] //section or something else?
-        
-        
-        
         //for each row, give me a particular cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "JobTableViewCell") as! JobTableViewCell
         
+        //READING FROM THE DATABASE WRONG - LOOK AT QUERY
+        //jobs.count is 0 right now
+        let job = jobs[indexPath.row] //section or something else?
+
         //now we can set information in each cell
         cell.JobNameLabel.text = job["name"] as! String
         cell.AssignedNameLabel.text = job["assigned_to"] as! String
         cell.DateLabel.text = job["due_date"] as! String
-        
+
         //add some logic for the status button image here
         //if is complete = 1 green
         //if not, yellow?
@@ -45,41 +70,13 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
             //completed. make color green
             cell.StatusImage.image = UIImage(named: "completed_status_image")
         }
-        
-        
-        
+
         return cell
     }
     
     
     @IBOutlet weak var tableView: UITableView!
     
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        //query data from the database
-        let query = PFQuery(className:"Job")
-        query.order(byDescending: "createdAt")
-        query.findObjectsInBackground { jobs, error in
-           if jobs != nil {
-               self.jobs = jobs!
-               self.tableView.reloadData()
-           }
-        }
-    }
-    
-
     @IBAction func onLogout(_ sender: Any) {
         PFUser.logOut()
         
